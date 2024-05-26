f0fe4b809f7d717b1abf3387e22040259569cdbd
mahnoor <157253733+mahnoor2289@users.noreply.github.com>
2024-05-26 11:49:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,287 @@
+// chapter no 1
+// ques no 1
+alert("welcome to our website");
+
+// ques no 2
+alert("Error !please enter a valid password");
+
+// ques no 3
+alert("welcome to JS land...\n Happy Coding!");
+
+// ques no 4
+alert("Welcome to our JS land...");
+
+alert("Happy Coding!\n Prevent this page from creating additional dialogs");
+
+
+// ques no 5
+alert(" Hello...I can run JS through my web browser's console");
+
+// ques no 6
+// done
+
+// ques no 7
+// done 
+
+// chapter no 2
+// Ques NO 1
+
+// ----varusername
+// -----let username
+
+// ques no 2
+
+let username = "MAHNOOR";
+
+// ques no 3
+let user = "HELLO WORLD";
+
+// Ques NO 4
+let studentname = "mahii";
+alert(studentname);
+
+let studentage = 20;
+alert(studentage);
+
+let itcourse = "web and app development";
+alert(itcourse);
+
+// Ques NO 5
+alert("PIZZA\n PIZZ \n PIZ\n PI\n A");
+
+// Ques NO 6
+
+let email = "noor55@gmail.com";
+alert(email);
+
+// ques no 7
+let book = "A SMARTER WAY TO LEARN JAVA SCRIPT ";
+alert(book);
+// quiz no 8
+document.write("yah! I can write HTML through JAVA SCRIPT" + "<br>");
+
+// Ques NO 9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+
+// CHAPTER NO 3
+// QUes NO 1
+let age = 15;
+alert(age);
+
+//  ques no 2
+
+let visitors = 7;
+alert("You have visited this site " + visitors + "times");
+
+// ques no 3
+let birthyear = 2002;
+document.write("my birth year is " + birthyear + "<br>" + "Data  type of my declared variable is " + typeof birthyear + "<br>");
+
+// ques no 4
+let buyer = "mahii";
+let product = "T-shirt";
+let quantity = 5;
+document.write(buyer + " ordered " + quantity + product + " on XYZClothing store  " + "<br>");
+
+//  chapter 4
+
+
+// // ques no 1
+// let a, b,c;
+// // quiz no 2
+// //  Legal variable names
+// my_var = 10
+// num_of_students = 20
+// total_sum = 100
+// first_name = "John"
+// _age = 30
+
+//      Illegal variable names
+//  2nd_var = 5  # Cannot start with a number
+//  my-var = 7   # Cannot contain special characters like '-'
+//  total sum = 50  # Cannot contain spaces
+//  for = "js"  # 'class' is a reserved keyword in JavaScript
+//  $money = 100  # Cannot start with special characters other than underscore '_'
+
+// ques no 3
+// Writing HTML content directly to the document
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain letters, numbers, underscores, and dollar signs ($).<br>For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a letter, underscore, or dollar sign.<br>For example $name, _name or name</p>");
+document.write("<p>Variable names are case sensitive</p>");
+document.write("<p>Variable names should not be JS keywords</p>");
+
+
+// chapter no 5
+// ques no 1
+let num1 = 3;
+let num2 = 5;
+
+let result = num1 + num2;
+
+document.write(" SUM of " + num1 + " and " + num2 + " is " + result + "<br>");
+
+// ques no 2
+let num3 = 9;
+let num4 = 5;
+
+let sub = num3 - num4;
+document.write(" Subtract of " + num3 + " and " + num4 + " is " + sub + "<br>");
+
+
+let num5 = 9;
+let num6 = 5;
+
+let multiply = num5 * num6;
+document.write(" multiply of " + num5 + " and " + num6 + " is " + multiply + "<br>");
+
+
+let num7 = 50;
+let num8 = 5;
+
+let divide = num7 / num8;
+document.write(" divide of " + num7 + " and " + num8 + " is " + divide + "<br>");
+
+// ques no 3
+
+document.write("<h3> ques 3<h3/>");
+// a:declare a variable
+let num;
+// b .show the value of variable after declaration
+document.write("value after variation declaration is : " + num + "<br>");
+// c.initialize the variable with some number
+num = 5;
+// d. show the initial value of the variable
+document.write("initial value:" + num + "<br>");
+// e.increment the variable
+num++;
+// f.show the value of variable after increment
+document.write("value after increment is :" + num + "<br>");
+// g.add 7 to the variable 
+num += 7;
+// h.show the value of variable after addition
+document.write("value after addition is :" + num + "<br>");
+
+// i.decrement the variable 
+num--;
+// j.show the value of variable after decrement 
+document.write("value after decrement is :" + num + "<br>");
+// k.show the remainder after dividing the variable's value by 3
+let remainder = num % 3;
+// l.show the remainder 
+document.write("the remainder is :" + remainder + "<br>");
+
+
+// ques 4
+
+document.write("<h3> ques 4 </h3>");
+let ticket = 600;
+let totalcost = ticket * 5;
+document.write("total cost of 5 movie tickets is :" + totalcost + "<br>");
+
+// ques 5
+
+document.write("<h3> ques 5 </h3>");
+document.write("<h2> table </h2>");
+document.write('2*1=', 2 * 1, '<br>')
+document.write('2*2=', 2 * 2, '<br>')
+document.write('2*3=', 2 * 3, '<br>')
+document.write('2*4=', 2 * 4, '<br>')
+document.write('2*5=', 2 * 5, '<br>')
+document.write('2*6=', 2 * 6, '<br>')
+document.write('2*7=', 2 * 7, '<br>')
+document.write('2*8=', 2 * 8, '<br>')
+document.write('2*9=', 2 * 9, '<br>')
+document.write('2*10=', 2 * 10, '<br>')
+
+// ques 6
+//    document.write("<h3> ques 6 </h3>");
+//   convert into celcios to faren
+//  let C=30;
+//  let f = (C*9/5)=32;
+//  document.write(C+'<sup>o</sup>C is '+f+'<sup>o</sup>F' );
+
+
+
+// Ques NO 7
+document.write("<h3>ques 7 </h3>");
+document.write("<h4> shopping cart </h4>");
+
+let item1 = 650;
+let item2 = 100
+let quantityitem1 = 3;
+let quantityitem2 = 7;
+let shipcharges = 100;
+let totalamount = quantityitem1 * item2 + quantityitem2 + shipcharges;
+
+document.write("price of item1 is ;" + item1 + "<br>" + "quantity of item1 is :" + quantityitem1 + "<br>");
+document.write("price of item2 is ;" + item2 + "<br>" + "quantity of item2 is :" + quantityitem2 + "<br>");
+document.write("shipping charges:" + totalamount + "<br>");
+
+document.write("total cost of your order is :" + totalamount + "<br>");
+
+
+// ques no 8
+
+document.write("<h3> ques 8 </h3>");
+document.write("<h3> mark sheet </h3>");
+let totalmarks = 850;
+let obtainmarks = 601;
+let result1 = obtainmarks / totalmarks * 100;
+document.write("total marks :" + totalmarks + "<br>");
+document.write("obtainmarks:" + obtainmarks + "<br>");
+document.write("percentage of result1 is :" + result1 + "<br>");
+
+// ques no 9
+document.write("<h3> ques 9 </h3>");
+let usdollars = 10;
+let saudiriyals = 25;
+let usdtopkrrate = 104.80;
+let sartopkrrate = 28
+
+let totalpkr = (usdollars * usdtopkrrate) + (saudiriyals * sartopkrrate);
+
+document.write("total currencyin pakistani rupees: " + totalpkr + "pkr");
+
+// // // //QUes NO 10
+document.write("<h3> ques 10 </h3>");
+
+let number = 5;
+let calculate = ((number + 5) * 10) / 2;
+document.write("Single expression final result is: " + calculate + "<br>");
+
+// // ques no 11
+document.write("<h3> ques no 11 </h3>");
+document.write("<h4> age calculator </h4>");
+let currentyear = 2024;
+let BYEAR = 2001;
+let Age = currentyear - BYEAR;
+document.write("current year:" + currentyear + "<br>" + "birth year:" + BYEAR + "<br>" + "age is:" + Age + "<BR>");
+
+
+
+// ques no 12
+document.write("<h3> ques no 12 </h3>");
+let r = 20;
+let pi = 3.142;
+let circumference = 2 * pi * r;
+let area = pi * (r * r);
+document.write("radius of a circle:" +r+" < br >" +"the circumference is :" + circumference + "<br>" + "the area is :" + area + "<br>");
+
+
+// // ques no 13
+document.write ("<h3> ques no 13 </h3>");
+ document.write("<h4> the life time supply cal! </h4>");
+ let favsnack="haleem";
+ let currentage=20;
+let maxage=55;
+ let amountperday=7;
+
+ let yearsremaining=maxage-currentage;
+ let totalneeded = yearsremaining * amountperday;
+ document.write("favsnack:"+favsnack+"<br>"+"the currentage is :"+currentage+"<br>"+"maxage:"+maxage +"<br>"+
+    "amount of snacks per day:"+amountperday+"<br>");
+
+ document.write("you need:"+totalneeded+"to last you ripe old age of: "+maxage+"<br>");
